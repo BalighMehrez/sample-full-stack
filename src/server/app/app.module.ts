db2114b9d8df29ac7809329676329f63fe89c0a1
Baligh Mehrez <bmehrez.contractor@circleup.com>
2020-03-31 15:05:41 +0200

    adding repo

--- src/server/app/app.module.ts
+++ src/server/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import RepoModule from './repo.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(),
+    RepoModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+

034aec78a271b3378bdb6017957eb3c769725ad0
Baligh Mehrez <bmehrez.contractor@circleup.com>
2020-03-31 13:22:41 +0200

    add migrations

---
+++ src/server/app/db/migrations/1585653638976-CreateBook.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateBook1585653638976 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<any> {
+    }
+
+}
